f210820a9c49c5294cc1c407920443cd6bb7398f
ASHUTOSH DEVGOTRA <146065904+AshutoshDevgotra@users.noreply.github.com>
2025-02-08 09:26:51 +0000

    expert online/offline

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -4,7 +4,8 @@ import useUpdateExpertStatus from "@/hooks/useUpdateExpertStatus";
 function MyApp({ Component, pageProps }: AppProps) {
   useUpdateExpertStatus(); // Call the hook to track status globally
 
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
+  ;
 }
 
 export default MyApp;
--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -4,6 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+  rel="preload"
+  href="/_next/static/media/a34f9d1faa5f3315-s.p.woff2"
+  as="font"
+  type="font/woff2"
+  crossOrigin="anonymous"
+/>
       <body>
         <Main />
         <NextScript />
